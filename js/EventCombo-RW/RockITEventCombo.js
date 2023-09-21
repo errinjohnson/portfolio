860952c2c5033503c39bbed1fe33a1f3faa6c1fc
errinjohnson <sporttrac64@outlook.com>
2023-09-21 06:02:37 -0500

    updated image link in js file

--- js/EventCombo-RW/RockITEventCombo.js
+++ js/EventCombo-RW/RockITEventCombo.js
@@ -25,7 +25,7 @@ function addElement() {
     newContent.setAttribute("class", "mx-auto");
 
     let newContent2 = document.createElement("img");
-    newContent2.src = "https://cdn.jsdelivr.net/gh/errinjohnson/portfolio@main/img/EventCombo-RW/RockIT_Logo.jpg";
+    newContent2.src = "https://raw.githubusercontent.com/errinjohnson/portfolio/e7deb200a6f3fee0c0d7a901d4975b567e10cd8b/img/EventCombo-RW/RockIT_Logo.jpg";
     newContent2.setAttribute("class", "rounded mx-auto d-block");
 
     newContent.appendChild(newContent2);
